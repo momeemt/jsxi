a2ef10488518cd3f1d58efa7cd9e1bc661ff632c
Mutsuha Asada <momiimt@gmail.com>
2021-12-27 03:48:34 +0900

    👍 Add: Get, Post of Posts

--- backend/api/posts.go
+++ backend/api/posts.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"momeemt/jsxi/lib"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
 type (
 	Post struct {
+		Id          int    `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
 		Name        string `json:"name"`
 		Code        string `json:"code"`
 		Description string `json:"description"`
@@ -12,3 +20,32 @@ type (
 		Posts []Post `json:"posts"`
 	}
 )
+
+func GetPosts(c echo.Context) error {
+	gormDB, sqlDB, err := lib.GetDBClient()
+	if err != nil {
+		return c.String(http.StatusServiceUnavailable, err.Error())
+	}
+	defer sqlDB.Close()
+	posts := []Post{}
+	query := gormDB.Model(&Post{})
+	query.Find(&posts)
+	response := new(Response)
+	response.Posts = posts
+
+	return c.JSON(http.StatusOK, response)
+}
+
+func PostPosts(c echo.Context) error {
+	post := Post{}
+	if err := c.Bind(&post); err != nil {
+		return c.String(http.StatusServiceUnavailable, err.Error())
+	}
+	gormDB, sqlDB, err := lib.GetDBClient()
+	if err != nil {
+		return c.String(http.StatusServiceUnavailable, err.Error())
+	}
+	defer sqlDB.Close()
+	gormDB.Create(&post)
+	return c.JSON(http.StatusCreated, post)
+}

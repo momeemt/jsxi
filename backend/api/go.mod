6eb3b95e93ea747319e66cc8bbdb1f8507fe618f
Mutsuha Asada <momiimt@gmail.com>
2021-12-26 15:14:18 +0900

    👍 Add: implement goenv

--- backend/api/go.mod
+++ backend/api/go.mod
@@ -1,5 +1,8 @@
-module your_module_name
+module jsxiapi
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.6.1
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/labstack/echo/v4 v4.6.1
+)
--- backend/api/main.go
+++ backend/api/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
+	"os"
 
-    "github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-    e := echo.New()
-    e.GET("/", index)
-    e.Logger.Fatal(e.Start(":8000"))
+	godotEnvErr := godotenv.Load(".env")
+	if godotEnvErr != nil {
+		panic("Error loading .env file")
+	}
+	googleClientID := os.Getenv("GoogleClientID")
+	googleClientSecret := os.Getenv("GoogleClientSecret")
+	fmt.Println(googleClientID, googleClientSecret)
+
+	e := echo.New()
+	e.GET("/", index)
+	e.Logger.Fatal(e.Start(":8000"))
 }
 
 func index(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello!")
+	return c.String(http.StatusOK, "Hello!")
 }
